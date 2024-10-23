32a037303657b1b309870f56dc8a23c48857e46b
Ken Fong Ka Kin <134134642+Kenfong0103@users.noreply.github.com>
2024-10-23 12:23:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ def fetch_existing_data():
     return existing_data
 
 # Create the Streamlit app
-st.markdown('<p>Health Prediction App</p>', unsafe_allow_html=True)
+st.title("Health Prediction App")
 
 # Define mappings for categorical variables
 glucose_mapping = {
