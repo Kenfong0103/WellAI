df60379647df83af2451bf36fdb689e2c3100497
Ken Fong Ka Kin <134134642+Kenfong0103@users.noreply.github.com>
2024-10-23 12:28:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(layout="wide")
 st.markdown("""
     <style>
     .big-font {
-        font-size:30px !important;
+        font-size:25px !important;
     }
     </style>
     """, unsafe_allow_html=True)
