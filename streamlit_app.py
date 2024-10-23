177545194b026e6fd8be552813c3dba6b46e70a0
Ken Fong Ka Kin <134134642+Kenfong0103@users.noreply.github.com>
2024-10-23 12:31:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ with st.form(key="user_form"):
         HbA1c_level = st.number_input('HbA1c Level', min_value=0.0, max_value=20.0, value=0.0)
 
     # Columns for submit button
-    col4, col5 = st.columns([20, 5])  # Adjust these values to control alignment
+    col4, col5 = st.columns([200, 5])  # Adjust these values to control alignment
 
     with col5:
         submit_button = st.form_submit_button(label="Submit")
